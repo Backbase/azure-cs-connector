b85be4dd48990605a1ef7590af19d16ce7c1b504
Afsin Kapusuzoglu <afsin@backbase.com>
2023-08-24 11:05:17 +0200

    feat: ssdk 16 update

--- src/main/java/com/backbase/communication/AzureCSConnectorApplication.java
+++ src/main/java/com/backbase/communication/AzureCSConnectorApplication.java
@@ -1,10 +1,6 @@
 package com.backbase.communication;
 
-import com.backbase.buildingblocks.commns.service.MessageSenderService;
-import com.backbase.buildingblocks.commns.service.StreamSender;
-import com.backbase.buildingblocks.commns.service.TrackingService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAsync
-@ImportAutoConfiguration(value = {TrackingService.class, MessageSenderService.class, StreamSender.class})
+//@ImportAutoConfiguration(value = {TrackingService.class, MessageSenderService.class, StreamSender.class}) //TODO should not be needed after ssdk-16 version of communication-channel library
 public class AzureCSConnectorApplication extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
